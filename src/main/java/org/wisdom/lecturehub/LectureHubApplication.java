4ddbc4df08726de7480bcff7db748a220a464ecc
wisdom <wisdom08@kakao.com>
2024-10-01 15:56:32 +0900

    Initial commit

---
+++ src/main/java/org/wisdom/lecturehub/LectureHubApplication.java
@@ -0,0 +1,13 @@
+package org.wisdom.lecturehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wisdom/lecturehub/LectureHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wisdom.lecturehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
